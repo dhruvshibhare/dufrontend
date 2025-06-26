da9571f90a7596e2594c625d378195ec0808be65
dhruvshibhare <shivharedhruv70@gmail.com>
2025-06-26 23:34:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   backendUrl: process.env.NODE_ENV === 'production'
-    ? 'https://your-backend-url.vercel.app'  // Replace with your actual backend URL after deployment
+    ? 'https://dubackend.vercel.app/'  // Replace with your actual backend URL after deployment
     : 'http://localhost:3001'
 };
 
